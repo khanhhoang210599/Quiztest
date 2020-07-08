8106193535b7a8e7e978ed5dee25a2009aef9e3d
khanhhoang210599 <57130524+khanhhoang210599@users.noreply.github.com>
2020-07-08 09:40:06 +0700

    Initial Commit

---
+++ Quiztest/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Quiztest
+//
+//  Created by admin on 5/15/20.
+//  Copyright © 2020 Khanhdepgai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: Any)
+    {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
